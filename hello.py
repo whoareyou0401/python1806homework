cf12567f71f0bb8beeec454b3322b98fbfb2bdea
ubuntu <ubuntu@localhost.localdomain>
2018-06-19 21:46:13 +0800

    sss

--- hello.py
+++ hello.py
@@ -2,5 +2,4 @@ print("first")
 print("JinGuangzhu is a fool") 
 print("To see a world in a grain of sand. And a heaven in a wild flower 
  Hold infinity in the palm of your hand. And eternity in an hour")
-print('嘿嘿')
 print('test')
