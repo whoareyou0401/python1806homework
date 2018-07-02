2fb58f54644d9efc26afc57b48fdb72791c2659b
daybaiday <15247328203@163.com>
2018-07-02 22:04:36 +0800

    add

---
+++ AXF/axfapp/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from axfapp import models
+# Create your views here.
+
+def home(req):
+    title = "首页"
+    swipers = models.Wheel.objects.all()
+    data = {
+        'title': title,
+        'swipers': swipers
+    }
+    return render(req,'home/home.html',context=data)
+
+def market(req):
+    return render(req,'market/market.html')
+
+def cart(req):
+    return render(req,'cart/cart.html')
+
+def mine(req):
+    return render(req,'mine/mine.html')
